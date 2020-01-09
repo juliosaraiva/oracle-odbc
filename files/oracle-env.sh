9bbbd078ad29a7419837c06293a36224b944d978
Julio Saraiva <contato@juliosaraiva.com.br>
2020-01-09 14:34:44 -0300

    First Commit

---
+++ files/oracle-env.sh
@@ -0,0 +1,9 @@
+ORACLE_HOME=/usr/lib/oracle/18.5/client64
+LD_LIBRARY_PATH=/usr/lib/oracle/18.5/client64/lib:/usr/lib
+TNS_ADMIN=/etc/oracle
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/oracle/18.5/client64/lib
+
+export ORACLE_HOME
+export LD_LIBRARY_PATH
+export TNS_ADMIN
+export PATH
